cf3cf2c2272b42a39f0a52d2e7dc26905da332d4
Drishti <82228411+Drishti9421@users.noreply.github.com>
2022-11-18 23:42:08 +0530

    Login and SignUp Pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import "./App.css";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AcidBaseDisorder from "./pages/AcidBaseDisorder";
+import Login from "./pages/Login/Login";
 import Navbar from "./components/Navbar";
+import SignUp from "./pages/SignUp/SignUp";
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/acidbase" element={<AcidBaseDisorder />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,14 +7,17 @@ const Navbar = () => {
         <img className="w-[50px]" src={logo} />
         <h1 className="font-bold text-2xl text-[#3A8EF6] ml-4">HealthyMe</h1>
       </div>
-      <div className="flex flex-1 justify-around items-center">
-        <h1 className="text-lg font-semibold">Home</h1>
-        <h1 className="text-lg font-semibold">About Us</h1>
-        <h1 className="text-lg font-semibold">How to use</h1>
+      
+        <div className="flex justify-around items-center">
+        <h1 className="text-lg font-semibold pr-5">Home</h1>
+        <h1 className="text-lg font-semibold px-10">About Us</h1>
+        <h1 className="text-lg font-semibold pl-5">How to use</h1>
+        </div>
+        <div>
         <button className="font-bold text-white bg-[#3A8EF6] px-8 py-2 rounded">
           Log In
         </button>
-      </div>
+        </div>
     </div>
   );
 };
