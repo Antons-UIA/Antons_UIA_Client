999eb8a23ca77a2a45a0430055187d0f8bea22fa
Drishti <82228411+Drishti9421@users.noreply.github.com>
2022-11-23 09:32:18 +0530

    VideoCall Feature

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Navbar from "./components/Navbar";
 import SignUp from "./pages/SignUp/SignUp";
 import Statistics from "./pages/Statistics";
 import Reports from "./pages/Reports";
+import VideoCall from "./components/VideoCall/VideoCall";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Router>
         <Routes>
+          <Route path="/video" element={<VideoCall />} />
           <Route path="/reports" element={<Reports />} />
           <Route path="reports/:id" element={<Statistics />} />
           <Route path="/acidbase" element={<AcidBaseDisorder />} />
---
+++ src/pages/Video/Video.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const urls = 'https://healthyme.daily.co/hello-daily'
+const Video = () => {
+    return (
+        <div>
+            <iframe src="https://antons.whereby.com/63b286ed-7736-4f15-8900-41ba538a9c2d" allow="camera; microphone; fullscreen; speaker; display-capture" style={{ height: "700px", width: "100%" }}></iframe>
+        </div>
+    )
+}
+
+export default Video
